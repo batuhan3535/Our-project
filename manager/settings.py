f584cf3f511c121f8bf0d0fbd2cecd8a723c2718
*Batuhan <batuhansakarya2712@gmail.com>
2021-08-11 11:10:02 +0200

    upgrated version

--- manager/settings.py
+++ manager/settings.py
@@ -14,6 +14,7 @@ import os
 
 
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -154,7 +155,7 @@ if os.getcwd() == '/app':
 
     # CONFIG FOR STATICFILES
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = 'static'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
